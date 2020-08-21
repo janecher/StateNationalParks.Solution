36a1ec6d8bf9059b7571f4d0145843fdb9b8aaf9
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-08-21 11:07:40 -0700

    add validation to classes in models, add class for secure storing API key

--- StateNationalParks/Models/NationalPark.cs
+++ StateNationalParks/Models/NationalPark.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StateNationalParks.Models
 {
     public class NationalPark
     {
         public int NationalParkId { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+        [StringLength(300)]
         public string Address { get; set; }
 
     }
