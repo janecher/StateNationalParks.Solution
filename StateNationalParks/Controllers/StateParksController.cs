632a6d38fbebf57b6e2186134f4f7d2f540dc60e
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-08-21 09:35:27 -0700

    add all CRUD routs to StateParksController and test them in Postman

---
+++ StateNationalParks/Controllers/StateParksController.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using StateNationalParks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace StateNationalParks.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class StateParksController : ControllerBase
+  {
+    private StateNationalParksContext _db;
+
+    public StateParksController(StateNationalParksContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/stateparks
+    [HttpGet]
+    public ActionResult<IEnumerable<StatePark>> Get()
+    {
+      return _db.StateParks.ToList();
+    }
+
+    // GET api/stateparks/5
+    [HttpGet("{id}")]
+    public ActionResult<StatePark> Get(int id)
+    {
+        return _db.StateParks.FirstOrDefault(entry => entry.StateParkId == id);
+    }
+
+    // POST api/stateparks
+    [HttpPost]
+    public void Post([FromBody] StatePark statePark)
+    {
+      _db.StateParks.Add(statePark);
+      _db.SaveChanges();
+    }
+
+    // PUT api/stateparks/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] StatePark statePark)
+    {
+        statePark.StateParkId = id;
+        _db.Entry(statePark).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+
+    // DELETE api/stateparks/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var stateParkToDelete = _db.StateParks.FirstOrDefault(entry => entry.StateParkId == id);
+      _db.StateParks.Remove(stateParkToDelete);
+      _db.SaveChanges();
+    }
+  }
+}
