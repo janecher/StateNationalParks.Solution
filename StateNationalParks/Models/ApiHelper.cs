8984d29e0803a0d64a0a88b875b36a1cba73e48c
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-08-21 12:15:26 -0700

    create ApiHelper class and create ApiCall method

---
+++ StateNationalParks/Models/ApiHelper.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace StateNationalParks.Models
+{
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://developer.nps.gov/api/v1/parks");
+      RestRequest request = new RestRequest($"?api_key={EnvironmentVariables.ApiKey}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
